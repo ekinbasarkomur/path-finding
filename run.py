04a6cddb787b904473b78087604032af17404e1c
Altan Kaplan <altanka@pop-os.localdomain>
2021-06-08 20:38:10 +0300

    add constant barriers, slow down drawing

--- run.py
+++ run.py
@@ -198,6 +198,8 @@ def draw(win, grid, rows, width):
 
 	draw_grid(win, rows, width)
 	pygame.display.update()
+	time.sleep(0.05)
+
 
 
 def get_clicked_pos(pos, rows, width):
@@ -214,11 +216,11 @@ def main(win, width):
 	ROWS = 20
 	grid = make_grid(ROWS, width, HEIGHT)
 
-	#select_barrier_spot = grid[5][20]
+	# select_barrier_spot = grid[12][20]
 	select_astar_spot = grid[5][20]
 	select_qlearning_spot = grid[10][20]
 
-	#select_barrier_spot.make_barrier()
+	# select_barrier_spot.make_barrier()
 	select_astar_spot.make_path()
 	select_qlearning_spot.make_closed()
 
@@ -230,11 +232,30 @@ def main(win, width):
 	barrier_select = False
 	astar_select = False
 	qlearning_select = False
+	bariers = [(6, 14), (7, 14), (8, 14), (9, 14), 
+					(10, 14), (11, 14), (12, 14), (13, 14), 
+					(6, 4), (6, 5), (6, 6), (6, 7), (6, 8), 
+					(7, 8), (7, 9), (7, 10), (7, 11), (6, 9), 
+					(6, 10), (6, 11), (11, 4), (12, 4), (13, 4), 
+					(14, 4), (15, 4), (16, 4), (17, 4), (14, 7), 
+					(14, 8), (14, 9), (14, 10), (15, 11), (15, 12), 
+					(14, 11), (14, 12), (15, 7), (16, 7), (17, 7),
+					(11, 7), (11, 8), (11, 9), (11, 10), (11, 11), 
+					(11, 12), (10, 12), (6, 12), (14, 2), (14, 3), 
+					(13, 2), (12, 2), (12, 1), (11, 1), (2, 1), (2, 2), 
+					(2, 3), (2, 4), (1, 4), (1, 5), (1, 6), (2, 13), 
+					(2, 14), (2, 15), (2, 16), (3, 16), (3, 17), (9, 17), 
+					(10, 17), (11, 17), (12, 17), (13, 17), (14, 17), (15, 17), 
+					(16, 17), (1, 9), (2, 9), (3, 9), (7, 0), (7, 1), (17, 0), 
+					(17, 1), (6, 19), (6, 18)]
+	
+	for (row, col) in bariers:
+		spot = grid[row][col]
+		spot.make_barrier()
 
 	while run:
 		draw(win, grid, ROWS, width)
 		event_list = pygame.event.get()
-
 
 		for event in event_list:
 			if event.type == pygame.QUIT:
@@ -260,6 +281,7 @@ def main(win, width):
 
 					if barrier_select:
 						spot.make_barrier()
+						
 					else:
 						if not end and spot != start:
 							end = spot
